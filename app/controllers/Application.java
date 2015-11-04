ece36f59d1ffc22c31a9996d3c3958a998465b26
IvanG <ivaang94@gmail.com>
2015-11-04 11:32:43 -0700

    Formularios validados

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,5 @@
 package controllers;
-
+import play.data.validation.*;
 import play.*;
 import play.mvc.*;
 
@@ -14,7 +14,7 @@ public class Application extends Controller {
 	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
 	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
 	}
-    
+
     public static void index() {
         Post frontPost = Post.find("order by postedAt desc").first();
         List<Post> olderPosts = Post.find(
@@ -22,5 +22,21 @@ public class Application extends Controller {
         ).from(1).fetch(10);
         render(frontPost, olderPosts);
     }
+
+    public static void show(Long id) {
+	    Post post = Post.findById(id);
+	    render(post);
+	}
+
+	public static void postComment(Long postId, @Required String author, @Required String content) {
+	    Post post = Post.findById(postId);
+	    if(validation.hasErrors()) {
+	        render("Application/show.html", post);
+	    }
+	    post.addComment(author, content);
+	    flash.success("Thanks for posting %s", author);
+	    show(postId);
+	}
+
 
 }
