4c42680af4a231a0cd30ac9083b048a6b0d46f2e
IvanG <ivaang94@gmail.com>
2015-11-04 23:45:28 -0700

    Modulo YABE implementado

--- app/models/User.java
+++ app/models/User.java
@@ -4,13 +4,20 @@ import java.util.*;
 import javax.persistence.*;
  
 import play.db.jpa.*;
+import play.data.validation.*;
  
 @Entity
 public class User extends Model {
  
+    @Email
+    @Required
     public String email;
+    
+    @Required
     public String password;
+    
     public String fullname;
+    
     public boolean isAdmin;
     
     public User(String email, String password, String fullname) {
@@ -18,9 +25,13 @@ public class User extends Model {
         this.password = password;
         this.fullname = fullname;
     }
-
+    
     public static User connect(String email, String password) {
-	    return find("byEmailAndPassword", email, password).first();
-	}
+        return find("byEmailAndPassword", email, password).first();
+    }
+    
+    public String toString() {
+        return email;
+    }
  
 }
